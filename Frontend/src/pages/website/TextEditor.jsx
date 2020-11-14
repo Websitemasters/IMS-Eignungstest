58abad89a3cf108232d21666bb7057f7a324345c
AndreiRobertoOleniuc <andreioleniucroberto@gmail.com>
2020-11-14 13:44:39 +0100

    texteditor

--- Frontend/src/pages/website/TextEditor.jsx
+++ Frontend/src/pages/website/TextEditor.jsx
@@ -4,18 +4,29 @@ import axios from "axios";
 export default function TextEditor(){
     const [inputCode,setInputCode] = useState("");
     const [outPut,setCode] = useState("");
+    const setInput = (e) =>{
+        setInputCode(e.target.value);
+    }
     const runCode= async () =>{
-        axios.get("")
+        axios.post("http://localhost:8080/useParser",{
+            text:inputCode,
+        })
+        .then((response)=>{
+            console.log(response.data);
+        })
+        .catch((error)=>{
+            console.log(error);
+        })
     }
     return (
         <div className="coding">
             <h1>Text Editor</h1>
             <div>
-                <textarea rows={20} cols={50} defaultValue={""} />
-                <textarea rows={20} cols={50} defaultValue={""} />
+                <textarea rows={20} cols={50} defaultValue={inputCode} onChange={setInput} />
+                <textarea rows={20} cols={50} defaultValue={outPut} />
             </div>
             <br/>
-            <button>Run</button>
+            <button onClick={runCode}>Run</button>
         </div>
     )
 }
