b91925731ecb8618ecffe00812805dd272ba825e
AndreiRobertoOleniuc <andreioleniucroberto@gmail.com>
2020-11-18 12:28:33 +0100

    formating

--- Frontend/src/pages/website/TextEditor.jsx
+++ Frontend/src/pages/website/TextEditor.jsx
@@ -1,25 +1,25 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
-export default function TextEditor(){
-    const [inputCode,setInputCode] = useState("");
-    const [outPut,setCode] = useState("");
-    const setInput = (e) =>{
+export default function TextEditor() {
+    const [inputCode, setInputCode] = useState("");
+    const [outPut, setCode] = useState("");
+    const setInput = (e) => {
         setInputCode(e.target.value);
     }
-    const runCode= async () =>{
-        axios.post(`http://localhost:8080/useParser`,{
-            id:1,
-            text:inputCode,
+    const runCode = async () => {
+        axios.post(`http://localhost:8080/useParser`, {
+            id: 1,
+            text: inputCode,
         })
-        .then((response)=>{
-            console.log(response);
-            setCode(response.data);
-        })
-        .catch((error)=>{
-            console.log(error);
-            setCode("Error");
-        })
+            .then((response) => {
+                console.log(response);
+                setCode(response.data);
+            })
+            .catch((error) => {
+                console.log(error);
+                setCode("Error");
+            })
     }
     return (
         <div className="coding">
@@ -28,7 +28,7 @@ export default function TextEditor(){
                 <textarea rows={10} cols={30} defaultValue={inputCode} onChange={setInput} />
                 <textarea rows={10} cols={30} defaultValue={outPut} />
             </div>
-            <br/>
+            <br />
             <button onClick={runCode}>Run</button>
         </div>
     )
