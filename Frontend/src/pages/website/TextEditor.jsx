540fda175868bd025300d66b12af81d35f5cb38f
AndreiRobertoOleniuc <andreioleniucroberto@gmail.com>
2020-11-25 15:30:00 +0100

    whitespaces and new lines removed

--- Frontend/src/pages/website/TextEditor.jsx
+++ Frontend/src/pages/website/TextEditor.jsx
@@ -11,6 +11,9 @@ export default function TextEditor({sendLocation,id}) {
         setInputCode(e.target.value);
     }
     const runCode = async () => {
+        let nowhiteSpaceCode = inputCode;
+        nowhiteSpaceCode = nowhiteSpaceCode.replaceAll(/\s/g, '');
+        console.log(nowhiteSpaceCode);
         axios.post(`http://localhost:8080/useParser`, {
             id: 1,
             text: inputCode,
