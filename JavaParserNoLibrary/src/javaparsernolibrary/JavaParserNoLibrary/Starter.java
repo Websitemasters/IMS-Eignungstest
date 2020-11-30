f6e9e41fc9947bf9bf4ed7c007e4dbea0c70a49c
1810g <1810.grand@gmail.com>
2020-11-30 10:25:42 +0100

    Parser sollte funktionieren

--- JavaParserNoLibrary/src/javaparsernolibrary/JavaParserNoLibrary/Starter.java
+++ JavaParserNoLibrary/src/javaparsernolibrary/JavaParserNoLibrary/Starter.java
@@ -6,7 +6,7 @@
 package javaparsernolibrary.JavaParserNoLibrary;
 
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -21,32 +21,52 @@ public class Starter {
 	 * @param args the command line arguments
 	 */
 	public static void main(String[] args) {
-		ArrayList<String>befehle=new ArrayList();
-		ArrayList<String>befehle2=new ArrayList();
-		befehle.add("x=10;");
-		befehle.add("x*5;");
-		befehle.add("y=15;");
-		befehle.add("y*2;");
-		befehle.add("while(x<100){x=x+10;}");
-		befehle.add("return x");
-		for(String s:befehle){
-		String p=s.replaceAll(";", "");
-		befehle2.add(p);
+		ArrayList<String> befehle2 = new ArrayList();
+		List<String> befehle = new ArrayList();
+		befehle.add("x=5");
+		befehle.add("if(x<100)");
+		befehle.add("{x=5*x");
+		befehle.add("}return x");
+		int manySkip = 0;
+		for (int i = 0; i < befehle.size(); i++) {
+			if (befehle.get(i).contains("if")) {
+				String s = befehle.get(i);
+				i++;
+				while (!befehle.get(i).contains("}")) {
+					s = s.concat(befehle.get(i));
+					i++;
+				}
+				if (befehle.get(i).indexOf("}") == 0) {
+					s += "}";
+					befehle2.add(s);
+					befehle2.add(befehle.get(i).substring(1, befehle.get(i).length()));
+					i++;
+				}
+				else {
+					s += (befehle.get(i).substring(0, befehle.get(i).indexOf("}")));
+					befehle2.add(s);
+					befehle2.add(befehle.get(i).substring((befehle.get(i).indexOf("}") + 1), befehle.get(i).length() - 1));
+				}
+			}
+			else {
+				befehle2.add(befehle.get(i));
+			}
 		}
-		
-		System.out.println(doParsing(befehle2,new HashMap()));
-
-		
+		for (String s : befehle2) {
+			System.out.println(s);
+		}
+		System.out.println(doParsing(befehle2, new HashMap()));
 	}
 
-	private static double doParsing(List<String>befehle, Map<String, Double>variablen){
-		if(befehle.size()==1){
-			return (double)(Parser.eval(befehle.get(0),variablen)[0]);
-		}else{
-			Map<String,Double>retMap=(Map<String, Double>)Parser.eval(befehle.get(0),variablen)[1];
+	private static double doParsing(List<String> befehle, Map<String, Double> variablen) {
+		if (befehle.size() == 1) {
+			return (double) (Parser.eval(befehle.get(0), variablen)[0]);
+		}
+		else {
+			Map<String, Double> retMap = (Map<String, Double>) Parser.eval(befehle.get(0), variablen)[1];
 			befehle.remove(0);
-		return doParsing(befehle,retMap);	
+			return doParsing(befehle, retMap);
 		}
 	}
-	
+
 }
