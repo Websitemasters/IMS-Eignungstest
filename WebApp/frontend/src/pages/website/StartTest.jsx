38f75282f770190b2c48316c3dcdd2c0282fdad2
AndreiRobertoOleniuc <andreioleniucroberto@gmail.com>
2020-12-16 15:32:27 +0100

    starting page

---
+++ WebApp/frontend/src/pages/website/StartTest.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Home({ sendeAktivitaet, userID }) {
+  React.useEffect(() => {
+    sendeAktivitaet.sendeAktivitaet("/", userID);
+  }, []);
+  return (
+    <div className="home">
+      <div className="plate">
+        <div className="contentInfo1">
+          <div className="title">
+            <h1>Home</h1>
+          </div>
+          <div className="erklaerung">
+            <p>
+              Auf dieser Seite werden Schüler die möglichkeit haben einen Test durchzuführen welcher ihnen verrät ob sie zu der IMS pasen oder nicht
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
