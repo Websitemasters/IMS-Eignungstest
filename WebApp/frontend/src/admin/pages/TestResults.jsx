178b87f277b851189e96ed2b985671b836731f90
AndreiRobertoOleniuc <andreioleniucroberto@gmail.com>
2020-12-16 15:11:10 +0100

    test results

--- WebApp/frontend/src/admin/pages/TestResults.jsx
+++ WebApp/frontend/src/admin/pages/TestResults.jsx
@@ -4,13 +4,24 @@ import * as BiIcons from "react-icons/bi";
 
 function TestResults() {
     const [avg, setAvg] = React.useState();
+    const [testErg, setTestErg] = React.useState([]);
     React.useEffect(() => {
         getAvg();
+        getTestErg();
     }, [])
     const getAvg = async () => {
         axios.get("http://localhost:8080/admin/getAVGTesterg")
             .then((res) => {
                 setAvg(parseFloat(res.data).toFixed(2));
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    }
+    const getTestErg = async () => {
+        axios.get("http://localhost:8080/admin/getTestErg")
+            .then((res) => {
+                setTestErg(res.data);
             })
             .catch((error) => {
                 console.log(error);
@@ -22,15 +33,32 @@ function TestResults() {
                 <div className="title">
                     <h1>TestResults</h1>
                 </div>
-                <div className="content">
-                    <div className="left">
-                        <div className="avg">
-                            <BiIcons.BiStats size={50} />
-                            <h3>Test Durchschnitt</h3>
-                            <p>{avg}</p>
+                <div className="contentInfo1">
+                    <div className="avg">
+                        <BiIcons.BiStats size={50} />
+                        <h3>Test Durchschnitt</h3>
+                        <p>{avg}</p>
+                    </div>
+                    <div className="contentInfo2">
+                        <div className="tableHold">
+                            <table className="testErg">
+                                <thead>
+                                    <tr>
+                                        <th align="left">User</th>
+                                        <th align="left">Test Ergebnis</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {testErg.map((item) => (
+                                        <tr key={item.id}>
+                                            <td>{item.id}</td>
+                                            <td>{item.testresults}</td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
                         </div>
                     </div>
-                    <div className="right"></div>
                 </div>
             </div>
         </div>
