3c5744275868564a756340806be6f0ef8eba5698
1810g <1810.grand@gmail.com>
2020-12-16 13:27:12 +0100

    Revert "Commit before pull 16.12"
    This reverts commit dcc15b5b99834a3b60c517da0cc70ffeef72290d.

--- WebApp/src/main/java/i3a/asn/QuestionsAPI/QuestionController.java
+++ WebApp/src/main/java/i3a/asn/QuestionsAPI/QuestionController.java
@@ -25,53 +25,35 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class QuestionController {
+    private Logic logic = Logic.getInstance();
+    private StartParser pc;
 
-	private Logic logic = Logic.getInstance();
-	private StartParser pc;
+    @PostMapping("/useParser")
+    @CrossOrigin(origins = "http://localhost:3000")
+    public String parseInput(@RequestBody ParseModel code){
+        pc=new StartParser();
+        String lines[] = code.getText().split("\\r?\\n");
+        List<String> inputCode = new ArrayList<>();
+        for(String s: lines){
+            System.out.println(s);
+            inputCode.add(s);
+        }
 
-	@PostMapping("/useParser")
-	@CrossOrigin(origins = "http://localhost:3000")
-	public String parseInput(@RequestBody ParseModel code) {
-		pc = new StartParser();
-		String lines[] = code.getText().split("\\r?\\n");
-		List<String> inputCode = new ArrayList<>();
-		for (String s : lines) {
-			System.out.println(s);
-			inputCode.add(s);
-		}
+	    return pc.startParser(inputCode);
+    }
 
-		return pc.startParser(inputCode);
-	}
+    @GetMapping("/getAllItems")
+    @CrossOrigin(origins = "http://localhost:3000")
+    public ArrayList<Items> getAllItems(){
+        return logic.getAllItems();
+    }
 
-	@GetMapping("/getAllItems")
-	@CrossOrigin(origins = "http://localhost:3000")
-	public ArrayList<Items> getAllItems() {
-		return logic.getAllItems();
-	}
-
-	@PostMapping("/rechneEignung")
-	@CrossOrigin(origins = "http://localhost:3000")
-	public String rechneEignung(@RequestBody ArrayList<Items> items) {
-		return returnAnswer(items);
-	}
-
-	private String returnAnswer(ArrayList<Items> itemList) {
-		//maximale punktzahn ist 70
-		String retStr = "";
-		//Iteriert über Items und erstellt bei spezialfällen spezifische nachricht
-		if(itemList.get(0).getAntwort()>3) retStr+="Super! Du hast bereits Kentnisse in der Applikationsentwicklung, du wirst sehr warscheinlich Vorteile haben.\n";
-		double eignung = 0.0;
-		double maxAnswers = 0.0;
-		for (Items is : itemList) {
-			maxAnswers += (10 * is.getGewichtung());
-		}
-
-		for (Items i : itemList) {
-			eignung += (i.getGewichtung() * i.getAntwort());
-		}
-		double percentAnswer = (eignung * 100) / maxAnswers;
-
-		return "Deine Antworten decken sich zu: " + Double.toString(percentAnswer) + "% mit den Antworten von IMS Schülern";
-
-	}
+    @PostMapping("/rechneEignung")
+    @CrossOrigin(origins = "http://localhost:3000")
+    public String rechneEignung(@RequestBody ArrayList<Items> items){
+        if(items.size()!=0){
+            return "yeah";
+        }
+        return "nah";
+    }
 }
