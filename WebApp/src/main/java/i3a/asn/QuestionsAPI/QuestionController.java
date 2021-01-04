c28d294232ac2d862464fdca33f6522d29ad66f6
AndreiRobertoOleniuc <andreioleniucroberto@gmail.com>
2021-01-04 22:57:05 +0100

    läuft wieder

--- WebApp/src/main/java/i3a/asn/QuestionsAPI/QuestionController.java
+++ WebApp/src/main/java/i3a/asn/QuestionsAPI/QuestionController.java
@@ -51,15 +51,11 @@ public class QuestionController {
 		return returnAnswer(items,id);
 	}
 
-<<<<<<< HEAD
-	private String returnAnswer(ArrayList<Items> itemList) {
-		//maximale Punktzahl ist 70
-		String retStr = "Vielen Dank für deine Teilnahme \n";
-=======
+
 	private String returnAnswer(ArrayList<Items> itemList,long id) {
 		//maximale punktzahn ist 70
 		String retStr = "";
->>>>>>> 7352e4eb4dfeb42cd5d57d764b00a8600f6cd4a8
+
 		//Iteriert über Items und erstellt bei spezialfällen spezifische nachricht
 		if (itemList.get(0).getAntwort() > 7) {
 			retStr += "Du hast bereits Kentnisse in der Applikationsentwicklung, du wirst sehr warscheinlich Vorteile haben.\n";
@@ -86,23 +82,23 @@ public class QuestionController {
 			eignung += (i.getGewichtung() * i.getAntwort());
 		}
 		double percentAnswer = (eignung * 100) / maxAnswers;
-<<<<<<< HEAD
 		percentAnswer = Math.ceil(percentAnswer);
 		if (percentAnswer < 50 && (itemList.get(2).getAntwort() > 6 || itemList.get(4).getAntwort() > 6)) {
+			logic.auswertung(percentAnswer,id);
 			retStr += "\nInfolge deiner Antworten, wurde berechnet, dass du in einer anderen Schule oder Lehre warscheinlich besser aufgehoben wärst. Dies heisst jedoch nicht, dass du die Option IMS streichen solltest! Nimm doch an einem Infoabend teil oder vereinbare einen Schnuppertermin bei der IMS um einen genaueren Einblick zu bekommen. ";
-			return retStr;
+			return "Deine Antworten decken sich zu: " + Double.toString(percentAnswer) + "% mit den Antworten von IMS Schülern\n"+retStr;
 		}else if(percentAnswer<50){
-		return retStr+"\n Laut deiner Antworten könntest du für die IMS geeignet sein, jedoch ist dies nicht sicher. Um eine bessere Entscheidung fällen zu können, besuche doch einen Infoanlass oder vereinbare einen Schnuppertermin";}
-		else if(percentAnswer>80){
-			return retStr+"\n Laut unserer Auswertung passt du besonders gut in die IMS! Jedoch wurde dies nur anhand von ein paar wenigen Fragen entschieden. Um sicher zu sein, inwiefern die IMS zu dir passt, macht es Sinn einen Schnuppertermin zu vereinbaren oder an einem Infoabend teilzunehmen";
-			}else{
-			return retStr+"\n Du könntest dich in der IMS zuhause fühlen! Jedoch ist dies keine definitive Antwort. Um einen genaueren Blick in den Alltag eines IMS-Schülers zu bekommen, nimm doch an einem Infoabend teil oder melde dich für einen Schnuppertermin an";
+			logic.auswertung(percentAnswer,id);
+			return "Deine Antworten decken sich zu: " + Double.toString(percentAnswer) + "% mit den Antworten von IMS Schülern\n"+retStr+"\n Laut deiner Antworten könntest du für die IMS geeignet sein, jedoch ist dies nicht sicher. Um eine bessere Entscheidung fällen zu können, besuche doch einen Infoanlass oder vereinbare einen Schnuppertermin";
 		}
-=======
-		percentAnswer=Math.ceil(percentAnswer);
-		logic.auswertung(percentAnswer,id);
-		return "Deine Antworten decken sich zu: " + Double.toString(percentAnswer) + "% mit den Antworten von IMS Schülern\n"+retStr;
->>>>>>> 7352e4eb4dfeb42cd5d57d764b00a8600f6cd4a8
+		else if(percentAnswer>80){
+			logic.auswertung(percentAnswer,id);
+			return "Deine Antworten decken sich zu: " + Double.toString(percentAnswer) + "% mit den Antworten von IMS Schülern\n"+retStr+"\n Laut unserer Auswertung passt du besonders gut in die IMS! Jedoch wurde dies nur anhand von ein paar wenigen Fragen entschieden. Um sicher zu sein, inwiefern die IMS zu dir passt, macht es Sinn einen Schnuppertermin zu vereinbaren oder an einem Infoabend teilzunehmen";
+		}
+		else {
+			logic.auswertung(percentAnswer, id);
+			return "Deine Antworten decken sich zu: " + Double.toString(percentAnswer) + "% mit den Antworten von IMS Schülern\n" + retStr + "\n Du könntest dich in der IMS zuhause fühlen! Jedoch ist dies keine definitive Antwort. Um einen genaueren Blick in den Alltag eines IMS-Schülers zu bekommen, nimm doch an einem Infoabend teil oder melde dich für einen Schnuppertermin an";
+		}
 
 	}
 }
