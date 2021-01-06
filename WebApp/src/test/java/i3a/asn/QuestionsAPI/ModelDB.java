b9f359d677ae28cf235cf90a6aebb917889407b0
1810g <1810.grand@gmail.com>
2021-01-06 15:20:41 +0100

    Ausgabe beendet

---
+++ WebApp/src/test/java/i3a/asn/QuestionsAPI/ModelDB.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package i3a.asn.QuestionsAPI;
+
+import i3a.asn.Models.Admin.LogEintrag;
+import i3a.asn.Models.Items.Items;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author 1810g
+ */
+public class ModelDB {
+ public int addVisitor(){
+	 return 0;//todo
+ }	
+
+ public boolean logActivity(int user, String url){
+	 return false;//todo
+ }
+
+public ArrayList<LogEintrag>actLog(){
+
+	return new ArrayList<LogEintrag>();//todo
+}
+
+
+public int getBesucher(){
+	return 0;//todo
+}
+
+
+public ArrayList<Items>getItems(){
+	return new ArrayList<Items>();
+}
+
+
+
+
+ 
+}
---
+++ WebApp/src/test/java/i3a/asn/QuestionsAPI/QuestionControllerTest.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package i3a.asn.QuestionsAPI;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author 1810g
+ */
+public class QuestionControllerTest {
+	
+	public QuestionControllerTest() {
+	}
+
+	/**
+	 * Test of parseInput method, of class QuestionController.
+	 */
+	@Test
+	public void testParseInput() {
+	}
+
+	/**
+	 * Test of getAllItems method, of class QuestionController.
+	 */
+	@Test
+	public void testGetAllItems() {
+	}
+
+	/**
+	 * Test of rechneEignung method, of class QuestionController.
+	 */
+	@Test
+	public void testRechneEignung() {
+	}
+	
+}
