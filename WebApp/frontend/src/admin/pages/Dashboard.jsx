ecf3cebfa0880b155aa0c973186c2b1e109d5995
AndreiRobertoOleniuc <andreioleniucroberto@gmail.com>
2021-01-07 22:56:56 +0100

    dashboard design changes

--- WebApp/frontend/src/admin/pages/Dashboard.jsx
+++ WebApp/frontend/src/admin/pages/Dashboard.jsx
@@ -60,69 +60,73 @@ function Dashboard() {
                         <h1>Dashboard</h1>
                     </div>
                 </div>
-                <div className="contentInfo1">
-                    <div className="besucher">
-                        <RiIcons.RiUserSearchFill size={70} />
-                        <h4>Anzahl Besucher</h4>
-                        <p>{besucherAnzahl}</p>
-                    </div>
-                    <div className="durchgefuehrt">
-                        <RiIcons.RiSurveyLine size={70} />
-                        <h4>Durchgeführte Test</h4>
-                        <p>{durchgefuehrte}</p>
-                    </div>
-                    <div className="seitenAufrufe">
-                        <AIIcons.AiFillEye size={70} />
-                        <h4>Anzahl Seiten aufrufe</h4>
-                        <p>{seitenAufrufe}</p>
-                    </div>
-                    <div className="testErgebnisse">
-                        <div className="tableHold">
-                            <h4>Test Ergbisse</h4>
-                            <table className="testErg">
-                                <thead>
-                                    <tr>
-                                        <th align="left">User</th>
-                                        <th align="left">Test Ergebnis</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {testErg.map((item) => (
-                                        <tr key={item.id}>
-                                            <td>{item.id}</td>
-                                            <td>{item.testresults} %</td>
+                <div className="content">
+                    <div className="contentInfo1">
+                        <div className="top">
+                            <div className="besucher">
+                                <RiIcons.RiUserSearchFill size={70} />
+                                <h4>Anzahl Besucher</h4>
+                                <p>{besucherAnzahl}</p>
+                            </div>
+                            <div className="durchgefuehrt">
+                                <RiIcons.RiSurveyLine size={70} />
+                                <h4>Durchgeführte Test</h4>
+                                <p>{durchgefuehrte}</p>
+                            </div>
+                            <div className="seitenAufrufe">
+                                <AIIcons.AiFillEye size={70} />
+                                <h4>Anzahl Seiten aufrufe</h4>
+                                <p>{seitenAufrufe}</p>
+                            </div>
+                        </div>
+                        <div className="vpi">
+                            <h2>Anzahl abbrüchen der Testdurchführung pro URL</h2>
+                            <div className="vpiTablePlaceHolder">
+                                <table className="vpiTable">
+                                    <thead>
+                                        <tr>
+                                            <th align="left">URL</th>
+                                            <th align="left">Anzahl</th>
                                         </tr>
-                                    ))}
-                                </tbody>
-                            </table>
+                                    </thead>
+                                    <tbody>
+                                        {vpi.map((item) => (
+                                            <tr key={item.url}>
+                                                <td>{item.url}</td>
+                                                <td>{item.anzahl}</td>
+                                            </tr>
+                                        ))}
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className="contentInfo2">
-                    <div className="vpi">
-                        <h3>Anzahl abbrüchen der Testdurchführung pro URL</h3>
-                        <div className="vpiTablePlaceHolder">
-                            <table className="vpiTable">
-                                <thead>
-                                    <tr>
-                                        <th align="left">URL</th>
-                                        <th align="left">Anzahl</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {vpi.map((item) => (
-                                        <tr key={item.url}>
-                                            <td>{item.url}</td>
-                                            <td>{item.anzahl}</td>
+                    <div className="contentInfo2">
+                        <div className="testErgebnisse">
+                            <div className="tableHold">
+                                <h2>Test Ergbisse</h2>
+                                <table className="testErg">
+                                    <thead>
+                                        <tr>
+                                            <th align="left">User</th>
+                                            <th align="left">Test Ergebnis</th>
                                         </tr>
-                                    ))}
-                                </tbody>
-                            </table>
+                                    </thead>
+                                    <tbody>
+                                        {testErg.map((item) => (
+                                            <tr key={item.id}>
+                                                <td>{item.id}</td>
+                                                <td>{item.testresults} %</td>
+                                            </tr>
+                                        ))}
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div >
+        </div>
     )
 }
 
