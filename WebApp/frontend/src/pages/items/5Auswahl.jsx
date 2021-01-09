1def9a8a2c8b9c96642ca0a95893896853ee557b
AndreiRobertoOleniuc <andreioleniucroberto@gmail.com>
2021-01-09 14:54:50 +0100

    safe 5 choices antwort

--- WebApp/frontend/src/pages/items/5Auswahl.jsx
+++ WebApp/frontend/src/pages/items/5Auswahl.jsx
@@ -1,15 +1,58 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import Progressionbar from "../components/Progressionbar";
 
+var linkButtonStyles = [
+  {
+    antwort: 10,
+    style: "linkbutton"
+  },
+  {
+    antwort: 8,
+    style: "linkbutton"
+  },
+  {
+    antwort: 6,
+    style: "linkbutton"
+  },
+  {
+    antwort: 4,
+    style: "linkbutton"
+  },
+  {
+    antwort: 2,
+    style: "linkbutton"
+  }
+]
 export default function Question({ frage, nextPage, lastPage, items, setItems, sendeAktivitaet, userID, progress, setProgress }) {
-  const [styleProg, setStyleProg] = React.useState("");
-  React.useEffect(() => {
+  const [styleProg, setStyleProg] = useState("");
+  const [styleButtons, setStyleButtons] = useState(linkButtonStyles);
+  useEffect(() => {
     setStyleProg("progress");
     setProgress((nextPage - 2) * 10);
+    changeStyle();
     sendeAktivitaet.sendeAktivitaet(`/Items/${nextPage - 1}`, userID);
   }, []);
 
+  const changeStyle = () => {
+    if (items[nextPage - 2].antwort !== 0) {
+      setStyleButtons(
+        styleButtons.map((item) => {
+          if (items[nextPage - 2].antwort === item.antwort) {
+            return {
+              ...item,
+              style: "colored"
+            }
+          }
+          else {
+            return {
+              ...item
+            }
+          }
+        })
+      )
+    }
+  }
   const location = useLocation();
   var idOfQuestion = location.pathname.substring(7, location.pathname.length);
   var intId = parseInt(idOfQuestion);
@@ -39,19 +82,19 @@ export default function Question({ frage, nextPage, lastPage, items, setItems, s
             <p>{frage}</p>
           </div>
           <div className="auswahl">
-            <Link className="linkbutton" to={lastPage === "true" ? "/Ausgabe" : `/Items/${nextPage}`} id="5" onClick={add}>
+            <Link className={styleButtons[0].style} to={lastPage === "true" ? "/Ausgabe" : `/Items/${nextPage}`} id="5" onClick={add}>
               5. Trifft zu
             </Link>
-            <Link className="linkbutton" to={lastPage === "true" ? "/Ausgabe" : `/Items/${nextPage}`} id="4" onClick={add}>
+            <Link className={styleButtons[1].style} to={lastPage === "true" ? "/Ausgabe" : `/Items/${nextPage}`} id="4" onClick={add}>
               4. Ziemlich
             </Link>
-            <Link className="linkbutton" to={lastPage === "true" ? "/Ausgabe" : `/Items/${nextPage}`} id="3" onClick={add}>
+            <Link className={styleButtons[2].style} to={lastPage === "true" ? "/Ausgabe" : `/Items/${nextPage}`} id="3" onClick={add}>
               3. Ein bisschen
             </Link>
-            <Link className="linkbutton" to={lastPage === "true" ? "/Ausgabe" : `/Items/${nextPage}`} id="2" onClick={add}>
+            <Link className={styleButtons[3].style} to={lastPage === "true" ? "/Ausgabe" : `/Items/${nextPage}`} id="2" onClick={add}>
               2. Wenig
             </Link>
-            <Link className="linkbutton" to={lastPage === "true" ? "/Ausgabe" : `/Items/${nextPage}`} id="1" onClick={add}>
+            <Link className={styleButtons[4].style} to={lastPage === "true" ? "/Ausgabe" : `/Items/${nextPage}`} id="1" onClick={add}>
               1. Gar nicht
             </Link>
           </div>
